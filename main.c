3519e7c2423e63a698a034cb262d0196a99dfc77
El-Duckster <144563447+El-Duckster@users.noreply.github.com>
2026-03-14 16:32:15 +0100

    add the name formatting function

--- main.c
+++ main.c
@@ -8,13 +8,26 @@
 #define TESTS 13 // # of tests per students
 #define NAME_LEN 11  // Max name length (10 chars + 1 for '\0')
 
+// ************Helper Functions***************
+void formatName(char name[]) {
+    name[0] = toupper(name[0]);
+    for (int i = 1; name[i] != '\0'; i++) {
+        name[i] = tolower(name[i]);
+    }
+}
+
 //*************Main Function*******************
 int main(void){
-    
+
  //Variables
  char names[STUDENTS][11];
 int scores[STUDENTS][TESTS];
 int sums[STUDENTS];
+
+// test the formating function
+    char name[11] = "aLICE";
+    formatName(name);
+    printf("%s\n", name);
 
     return 0;
 }
