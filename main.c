b1f529daef6f91c91308fc0925dbedd6f3cc23a9
El-Duckster <144563447+El-Duckster@users.noreply.github.com>
2026-03-14 16:14:24 +0100

    add constants to avoid unreasonable numbers in the code

--- main.c
+++ main.c
@@ -3,7 +3,10 @@
 #include <stdio.h>
 #include <ctype.h>
 
-
+//**********Constants*****************
+#define STUDENTS 5 // # of students
+#define TESTS 13 // # of tests per students
+#define NAME_LEN 11  // Max name length (10 chars + 1 for '\0')
 
 //*************Main Function*******************
 int main(void){
