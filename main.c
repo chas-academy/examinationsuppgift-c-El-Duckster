832afc8c3a343f2939fe72ffbaa1a8c1de3c94d4
El-Duckster <144563447+El-Duckster@users.noreply.github.com>
2026-03-14 16:05:02 +0100

    Program skeleton + libraries

--- main.c
+++ main.c
@@ -1,2 +1,13 @@
 //Write your code in this file
+//*************Libraries*************
+#include <stdio.h>
+#include <ctype.h>
+
+
+
+//*************Main Function*******************
+int main(void){
+
+    return 0;
+}
 
