1196a7024c41ab5f8ac1c9461ba7dc6969b6fe8e
El-Duckster <144563447+El-Duckster@users.noreply.github.com>
2026-03-15 10:14:16 +0100

    add the sum function

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #define NAME_LEN 11  // Max name length (10 chars + 1 for '\0')
 
 // ************Helper Functions***************
+//name formatting
 void formatName(char name[]) {
     name[0] = toupper(name[0]);
     for (int i = 1; name[i] != '\0'; i++) {
@@ -16,18 +17,29 @@ void formatName(char name[]) {
     }
 }
 
+// sum up student scores
+int calculateSum(int scores[]) {
+    int sum = 0;
+    for (int i = 0; i < TESTS; i++) {
+        sum += scores[i];
+    }
+    return sum;
+}
 //*************Main Function*******************
 int main(void){
 
  //Variables
  char names[STUDENTS][11];
-int scores[STUDENTS][TESTS];
+// int scores[STUDENTS][TESTS];
 int sums[STUDENTS];
 
 // test the formating function
     char name[11] = "aLICE";
     formatName(name);
     printf("%s\n", name);
+// test the sum function
+ int scores[TESTS] = {10,10,10,10,10,10,10,10,10,10,10,10,10};
+    printf("%d\n", calculateSum(scores));
 
     return 0;
 }
